940adfd3be04a3e8ba5119cae86518deba083b40
William L <w-lindvall@outlook.com>
2019-03-10 00:16:31 -0500

    feat: reset css and style

--- ws.js
+++ ws.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000
 const INDEX = path.join(__dirname, '/src/index.html')
 
 const server = express()
+  .use(express.static(__dirname + '/src/static/'))
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
